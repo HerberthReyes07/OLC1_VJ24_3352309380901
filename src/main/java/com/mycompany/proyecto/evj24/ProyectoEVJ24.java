f8de33a37e804b069c7515402324a41dff0d0e1d
HerberthReyes07 <herberthreyes202230236@cunoc.edu.gt>
2024-06-03 22:28:39 -0600

    Inicializando proyecto

---
+++ src/main/java/com/mycompany/proyecto/evj24/ProyectoEVJ24.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto.evj24;
+
+/**
+ *
+ * @author herberthreyes
+ */
+public class ProyectoEVJ24 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
